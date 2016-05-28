450bc6cc723e50cddfa134f36784a42ba5b71f4e
Mark Gray <mrmarkgray@gmail.com>
2016-05-28 20:08:19 +0000

    timer: Add dummy implementation for testing
    Signed-off-by: Mark Gray <mrmarkgray@gmail.com>

--- haldane.py
+++ haldane.py
@@ -6,32 +6,32 @@ from model import *
 import time
 
 def main():
-    sensor = Sensor.factory("MS5803-14B")
+#    sensor = Sensor.factory("MS5803-14B")
+    sensor = Sensor.factory("Dummy")
     model = Model.factory("Buhlmann")
-    timer = Timer.factory("Sleep", 1)
+    timer = Timer.factory("Dummy", 60)
     display = Display.factory("Stdio")
 
+    ambient = 1.013
     print("- Starting Haldane -")
-    temp = sensor.getTemperature()
-    pres = sensor.getPressure()  
     
-    start_time = int(time.time())
-    model.reset(pres, 0)
+    model.reset(ambient, 0)
     
 
     while timer.wait():
         temp = sensor.getTemperature()
         pres = sensor.getPressure()  
-        current_time = int(time.time()) - start_time
-        depth = (pres - 1.013) * 10
+        current_time =  timer.getTime()
+        depth = (pres - ambient) * 10
         if depth < 0: depth = 0
 
-        model.update(pres, current_time)
+        print(depth, pres,current_time)
+        model.update(pres, current_time / 60 )
         ndl = model.ndl()
         if ndl == 99: ndl = "N/A"
+        if ndl == 0: break
 
         display.display(ndl, current_time, depth, temp)
-
        
 
 if __name__ == "__main__":
--- timer.py
+++ timer.py
@@ -12,10 +12,15 @@ class Timer(object):
     @abstractmethod
     def wait(self):
         pass
+     
+    @abstractmethod
+    def getTime(self):
+        pass
 
     @staticmethod
     def factory(type, interval):
         if type == "Sleep": return Sleep(interval)
+        if type == "Dummy": return Dummy(interval)
         assert 0, "Bad timer: " + type
 
 
@@ -26,16 +31,34 @@ class Sleep():
     def __init__(self, interval):
         self.interval = interval 
         self.last = int(time.time())
+        self.start = self.last
 
     def wait(self):
         pause.until(self.last + self.interval)
-        self.last = int(time.time())
+        self.last = self.last + self.interval
         return True
 
+    def getTime(self):
+        return int(time.time()) - self.start
+
+class Dummy():
+    """ Dummy implementation of class Timer for testing.
+
+    """
+    def __init__(self, interval):
+        self.interval = interval 
+        self.time = 0
+
+    def wait(self):
+        self.time = self.time + self.interval
+        return True
+
+    def getTime(self):
+        return self.time
 
 if __name__ == "__main__":
     print("Test Timer")
     timer = Timer.factory("Sleep", 1)
     for interval in range(0,9):
         timer.wait()
-        print(time.strftime("%S"), " seconds")
+        print(timer.getTime(), " seconds")
